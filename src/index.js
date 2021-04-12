05e3f8276e2b3a251dbcb10049bd3d6f981f3304
Chris Amyx <40224280+cdamyx@users.noreply.github.com>
2021-04-11 20:54:21 -0500

    date constructor function with full year instance!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 //It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+const fname = "Allen";
+const lname = "McStubbins";
+//call date as constructor to return a date object, then use desired instance on it
+const date = new Date();
+const year = date.getFullYear();
+ReactDOM.render(
+  <div>
+    <p>
+      Created by {fname} {lname}
+    </p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
